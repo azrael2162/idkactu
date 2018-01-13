29701f31ed3a286a8213ce16402ccccfd79d587a
Vincent <vincent.pinelli2162@gmail.com>
2018-01-13 15:58:02 +0100

    je sais pas

---
+++ vues/accueil.php
@@ -0,0 +1,11 @@
+<?php
+$url = "http://tonyarchambeau.com/feed/"; /* insérer ici l'adresse du flux RSS de votre choix */
+$rss = simplexml_load_file($url);
+echo '<ul>';
+foreach ($rss->channel->item as $item){
+ $datetime = date_create($item->pubDate);
+ $date = date_format($datetime, 'd M Y, H\hi');
+ echo '<li><a href="'.$item->link.'">'.utf8_decode($item->title).'</a> ('.$date.')</li>';
+}
+echo '</ul>';
+?>
