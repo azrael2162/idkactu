9b989db904296b2ed6acd2be743c10ce96261b1d
Vincent <vincent.pinelli2162@gmail.com>
2018-01-13 01:17:14 +0100

    second commit

---
+++ body/header.php
@@ -0,0 +1,40 @@
+<head>
+  <!--bootstrap-->
+  <link rel="stylesheet" href="css/flatly.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js" integrity="sha384-VspmFJ2uqRrKr3en+IG0cIq1Cl/v/PHneDw6SQZYgrcr8ZZmZoQ3zhuGfMnSR/F2" crossorigin="anonymous"></script>
+  <link rel="icon" height="16px" width="16px" href="image/ico/icon.ico" />
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><img height="61px" width="38px" src="image/logo/logo.png" alt=""> IDK.ACTU</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Reseaux/Sec</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Developpement</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Hardware</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" >|</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" >|</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Connexion</a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ pages/accueil.php
@@ -0,0 +1,10 @@
+<html>
+<?php include 'body/header.php'; ?>
+  <header>
+    <meta charset="utf-8">
+    <title>accueil</title>
+  </header>
+  <body>
+<?php include 'vues/accueil.php'; ?>
+  </body>
+</html>
